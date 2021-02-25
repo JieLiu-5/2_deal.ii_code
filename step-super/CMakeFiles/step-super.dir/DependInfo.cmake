b91966b857aaad937e9079eab42555b4088b6325
JieLiu-5 <j.liu-5@tudelft.nl>
2021-02-25 10:00:43 +0100

    copy step-super to Deal.ii-code

---
+++ step-super/CMakeFiles/step-super.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/LIUJI/Dropbox/step-super/step-super.cc" "/mnt/c/Users/LIUJI/Dropbox/step-super/CMakeFiles/step-super.dir/step-super.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BOOST_NO_AUTO_PTR"
+  "DEBUG"
+  "TBB_DO_ASSERT=1"
+  "TBB_USE_DEBUG"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../step-4/include"
+  "../step-20/include"
+  "/home/wsl_jie/dealii_920/include"
+  "/home/wsl_jie/dealii_920/include/deal.II/bundled"
+  "/usr/include/suitesparse"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
